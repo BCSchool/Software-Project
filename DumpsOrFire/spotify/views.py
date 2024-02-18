f88701463108a6940ebeff1274521966477507bd
Joshua Holman <joshuaholman5@gmail.com>
2024-02-17 20:22:30 -0800

    webpage up but no functionality

--- DumpsOrFire/spotify/views.py
+++ DumpsOrFire/spotify/views.py
@@ -6,4 +6,15 @@ def index(request):
     return render(request, 'spotify/index.html')
 
 def home(request):
-    return render(request, 'spotify/home.html')
+    rating = None
+    if request.method == 'POST':
+        playlist_name = request.POST.get('playlist_name')
+        rating = 7
+        # try:
+        #     context['rating'] = generate_rating(playlist_name)
+        #     raise PlaylistNotFound("Playlist not found :(")
+        # except PlaylistNotFound as e:
+        #     context['error'] = str(e)
+
+    # return render(request, 'spotify/home.html', context)
+    return render(request, 'spotify/home.html', {'rating': rating})
