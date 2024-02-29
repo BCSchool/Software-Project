74a42b7e665d6d45f8c1188585855597c8b49e17
Joshua Holman <joshuaholman5@gmail.com>
2024-02-28 19:43:42 -0800

    trying to get pythonanywhere hosting working

--- DumpsOrFire/DumpsOrFire/settings.py
+++ DumpsOrFire/DumpsOrFire/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_secret_key')
-
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
