e5efc48823be44f8d3fdd0c2a020af25b3c8c871
Joshua Holman <joshuaholman5@gmail.com>
2024-03-04 13:21:03 -0800

    updated allowed hosts

--- DumpsOrFire/DumpsOrFire/settings.py
+++ DumpsOrFire/DumpsOrFire/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jolman.pythonanywhere.com']
 
 # Spotify
 SOCIAL_AUTH_SPOTIFY_ID = os.environ.get('SPOTIFY_CLIENT_ID')
