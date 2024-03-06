ce53d721375a4d82533ecc6116910b1af9f24d58
Joshua Holman <joshuaholman5@gmail.com>
2024-03-06 15:58:10 -0800

    added dropdown menu (visual only)

--- DumpsOrFire/spotify/views.py
+++ DumpsOrFire/spotify/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import generate_rating as gr
+# from django.http import JsonResponse
 
 # Create your views here.
 
@@ -8,8 +9,15 @@ def index(request):
 
 def rate(request):
     context = {}
+    context['search_type'] = 'track'
+    # if request.method == 'GET':
+    #     search_type = request.GET.get('search_type', 'track')
+    #     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+    #         return JsonResponse({'search_type': search_type})
     if request.method == 'POST':
         user_input = request.POST.get('user_input')
+        search_type = request.POST.get('search_type', 'track')
+        context['search_type'] = search_type
         if gr.get_track_popularity(user_input) is not None:
             context['rating'] = gr.get_track_popularity(user_input)
             context['image'] = gr.get_track_image(user_input)
