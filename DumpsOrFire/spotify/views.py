da3194ed601ad2144069adc588d733a50e762ea4
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 <bschool3000@gmail.com>
2024-03-06 13:32:10 -0800

    format rating class added

--- DumpsOrFire/spotify/views.py
+++ DumpsOrFire/spotify/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import generate_rating as gr
+from . import format_rating as fr
 
 # Create your views here.
 
@@ -12,6 +13,7 @@ def home(request):
         user_input = request.POST.get('user_input')
         if gr.get_track_popularity(user_input) is not None:
             context['rating'] = gr.get_track_popularity(user_input)
+            context['description'] = fr.format_rating(gr.get_track_popularity(user_input))
             context['image'] = gr.get_track_image(user_input)
         else:
             context['error'] = f"No result with name {user_input} found."
