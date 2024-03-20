27619d6a503ecfe77c11990ae2b6fbf0f7d82594
Joshua Holman <joshuaholman5@gmail.com>
2024-03-20 13:18:50 -0700

    refactored static file management

--- DumpsOrFire/DumpsOrFire/settings.py
+++ DumpsOrFire/DumpsOrFire/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'spotify/static'),
-    os.path.join(BASE_DIR, 'static'),
+    # os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
