233fd8a94dcdc847b69ff6639bfd18129b1b79b4
Joshua Holman <joshuaholman5@gmail.com>
2024-03-27 11:18:03 -0700

    nonfunctional

--- DumpsOrFire/spotify/views.py
+++ DumpsOrFire/spotify/views.py
@@ -12,22 +12,28 @@ def index(request):
 
 def rate(request):
     context = {}
-    context['search_type'] = 'track'
     if request.method == 'POST':
+        '''Get user input and change search type text in search box'''
         user_input = request.POST.get('user_input')
         search_type = request.POST.get('search_type', 'album')
         context['search_type'] = search_type
-        if gr.get_track_popularity(user_input) is not None:
-            if search_type == 'track':
+
+        if search_type == 'track':
+            '''Track Search'''
+            if gr.get_track_popularity(user_input) is not None:
+                '''get rating from api and description from json file'''
                 context['rating'] = gr.get_track_popularity(user_input)
                 context['description'] =  fr.format_rating(gr.get_track_popularity(user_input))
                 # context['rating_reaction'] =  fr.get_rating_reaction(letter_rating='A')
                 context['image'] = gr.get_track_image(user_input)
+                context['name'] = gr.get_track_name(user_input)
+            else:
+                context['error'] = f"No result with name {user_input} found."
 
-            elif search_type == 'album':
+        elif search_type == 'album':
+            '''Album Search'''
+            if gr.get_album_popularity(user_input) is not None:
                 context['rating'] = gr.get_album_popularity(user_input)
-                context['description'] = "yo"
-        else:
-            context['error'] = f"No result with name {user_input} found."
+                context['description'] =  fr.format_rating(gr.get_album_popularity(user_input))
 
     return render(request, 'spotify/rate.html', context)
